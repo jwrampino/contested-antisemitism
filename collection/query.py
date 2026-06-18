3f5b651a29ad04ad490e0438aa32743c714c0101
jwrampino <jwrampino@uchicago.edu>
2026-06-18 12:53:27 -0500

    update query and add initial pagination logic

--- collection/query.py
+++ collection/query.py
@@ -1,7 +1,10 @@
 # query.py
 
-ANTI = '"jew*" OR "joos" OR "zio" OR "zios" OR ("ZOG" NOT ("und" OR "von" OR "sich" OR "zu" OR "nach" OR "der")) OR "NWO" OR "heil hitler" OR "holocaust" OR ("(((" AND ")))") OR "the JQ" OR "soros" OR "rothschild" OR "kike*"'
+# potentially jewish-targeted discourse
+ANTI = '"jew*" OR "joos" OR "jooish" OR "zio" OR "zios" OR "goyslop" OR ("ZOG" NOT ("und" OR "von" OR "sich" OR "zu" OR "nach" OR "der")) OR "NWO" OR "heil hitler" OR "holocaust" OR ("(((" AND ")))") OR "the JQ" OR "soros" OR "rothschild" OR "kike*"'
 
-ISRAEL = '"mossad" OR "aipac" OR "israel*" OR "zioni*"'
+# israeli political discourse
+ISRAEL = '"mossad" OR "aipac" OR "israel*" OR "zioni*" OR "netanyahu*"'
 
-PALESTINE = '"gaza" OR "palestin*"'
+# palestinian political discourse
+PALESTINE = '"gaza" OR "palestin*" OR "from the river to the sea" OR "hamas" OR "palestinian authority" OR "fatah"'
