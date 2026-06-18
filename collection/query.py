e15f40a688fcc80e28854da66f477cb04c69a3cd
jwrampino <jwrampino@uchicago.edu>
2026-06-18 15:58:13 -0500

    finalize pagination and query logic

--- collection/query.py
+++ collection/query.py
@@ -1,10 +1,10 @@
 # query.py
 
 # potentially jewish-targeted discourse
-ANTI = '"jew*" OR "joos" OR "jooish" OR "zio" OR "zios" OR "goyslop" OR ("ZOG" NOT ("und" OR "von" OR "sich" OR "zu" OR "nach" OR "der")) OR "NWO" OR "heil hitler" OR "holocaust" OR ("(((" AND ")))") OR "the JQ" OR "soros" OR "rothschild" OR "kike*"'
+ANTI = 'jew* OR joos OR jooish OR zio OR zios OR goyslop OR (ZOG AND NOT (und OR von OR sich OR zu OR nach OR der)) OR NWO OR "heil hitler" OR holocaust OR ("(((" AND ")))") OR "the JQ" OR soros OR rothschild OR kike*'
 
 # israeli political discourse
-ISRAEL = '"mossad" OR "aipac" OR "israel*" OR "zioni*" OR "netanyahu*"'
+ISRAEL = 'mossad OR aipac OR israel* OR zioni* OR netanyahu*'
 
 # palestinian political discourse
-PALESTINE = '"gaza" OR "palestin*" OR "from the river to the sea" OR "hamas" OR "palestinian authority" OR "fatah"'
+PALESTINE = 'gaza OR palestin* OR "from the river to the sea" OR hamas OR "palestinian authority" OR fatah'
